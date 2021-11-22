c7506e944a3a21d623e189f9f4800f72fd15b72a
YaroslavOnofriichuk <yaroslavonofriichuk@ukr.net>
2021-11-22 20:46:01 +0200

    Переродив дз 10

--- src/index.js
+++ src/index.js
@@ -16,17 +16,19 @@ const refs = {
 refs.input.addEventListener("input", debounce(searchCountries, DEBOUNCE_DELAY));
 
 function searchCountries (e) {
-    fetchCountries(e.target.value.trim()).then(data => {
-        if (data.length > 10) {
-            Notify.info("Too many matches found. Please enter a more specific name.");
-            refs.list.innerHTML = "";
-            refs.info.innerHTML = "";
-        } else if (data.length >= 2 && data.length <= 10) {
-            refs.list.innerHTML = createListMarkup(data);
-            refs.info.innerHTML = "";
-        }  else {
-            refs.info.innerHTML = createInfoMarkup(data);
-            refs.list.innerHTML = "";
-        }
-    }).catch((error) => Notify.failure("Oops, there is no country with that name"));
+    if (e.target.value.trim() !== "") {
+        fetchCountries(e.target.value.trim()).then(data => {
+            if (data.length > 10) {
+                Notify.info("Too many matches found. Please enter a more specific name.");
+                refs.list.innerHTML = "";
+                refs.info.innerHTML = "";
+            } else if (data.length >= 2 && data.length <= 10) {
+                refs.list.innerHTML = createListMarkup(data);
+                refs.info.innerHTML = "";
+            }  else {
+                refs.info.innerHTML = createInfoMarkup(data);
+                refs.list.innerHTML = "";
+            }
+        }).catch((error) => Notify.failure("Oops, there is no country with that name"));
+    }
 };
